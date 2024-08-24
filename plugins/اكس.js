cf1c8a3d865f6f9e6a6d6e09c0388053ce97c23a
Peto78uowhsl <hwff433@gmail.com>
2024-08-24 12:48:48 +0300

    Update اكس.js

--- plugins/اكس.js
+++ plugins/اكس.js
@@ -1,37 +1,38 @@
-import TicTacToe from '../lib/tictactoe.js'
+
+import TicTacToe from  ../lib/tictactoe.js 
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
     conn.game = conn.game ? conn.game : {}
-    if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) throw `❏ انت ماذلت في الجيم, لحذف الجيم اكتب : *${usedPrefix}حذففف*`
+    if (Object.values(conn.game).find(room => room.id.startsWith( tictactoe ) && [room.game.playerX, room.game.playerO].includes(m.sender))) throw `❏ انت ماذلت في الجيم, لحذف الجيم اكتب : *${usedPrefix}حذففف*`
     if (!text) throw `✳️ Put a number in the room`
-    let room = Object.values(conn.game).find(room => room.state === 'WAITING' && (text ? room.name === text : true))
-    // m.reply('[WIP Feature]')
+    let room = Object.values(conn.game).find(room => room.state ===  WAITING  && (text ? room.name === text : true))
+    // m.reply( [WIP Feature] )
     if (room) {
-        m.reply('*تم ايجاد الشخص الاخر*')
+        m.reply( *تم ايجاد الشخص الاخر* )
         room.o = m.chat
         room.game.playerO = m.sender
-        room.state = 'PLAYING'
+        room.state =  PLAYING 
         let arr = room.game.render().map(v => {
             return {
-                X: '❎',
-                O: '⭕',
-                1: '1️⃣',
-                2: '2️⃣',
-                3: '3️⃣',
-                4: '4️⃣',
-                5: '5️⃣',
-                6: '6️⃣',
-                7: '7️⃣',
-                8: '8️⃣',
-                9: '9️⃣',
+                X:  ❎ ,
+                O:  ⭕ ,
+                1:  1️⃣ ,
+                2:  2️⃣ ,
+                3:  3️⃣ ,
+                4:  4️⃣ ,
+                5:  5️⃣ ,
+                6:  6️⃣ ,
+                7:  7️⃣ ,
+                8:  8️⃣ ,
+                9:  9️⃣ ,
             }[v]
         })
         let str = `
-انتظر @${room.game.currentTurn.split('@')[0]} هو اللاعب الاول
+انتظر @${room.game.currentTurn.split( @ )[0]} هو اللاعب الاول
 
-${arr.slice(0, 3).join('')}
-${arr.slice(3, 6).join('')}
-${arr.slice(6).join('')}
+${arr.slice(0, 3).join(  )}
+${arr.slice(3, 6).join(  )}
+${arr.slice(6).join(  )}
 
 ▢ *Room ID* ${room.id}
 
@@ -46,11 +47,11 @@ ${arr.slice(6).join('')}
         })
     } else {
         room = {
-            id: 'tictactoe-' + (+new Date),
+            id:  tictactoe-  + (+new Date),
             x: m.chat,
-            o: '',
-            game: new TicTacToe(m.sender, 'o'),
-            state: 'WAITING'
+            o:   ,
+            game: new TicTacToe(m.sender,  o ),
+            state:  WAITING 
         }
         if (text) room.name = text
 
@@ -66,8 +67,8 @@ ${arr.slice(6).join('')}
 
 }
 
-handler.help = ['tictactoe <tag number>']
-handler.tags = ['game']
-handler.command = ['tictactoe', 'ttc', 'اكس', 'xo']
+handler.help = [ tictactoe <tag number> ]
+handler.tags = [ game ]
+handler.command = [ tictactoe ,  ttc ,  اكس ,  xo ]
 
 export default handler
